b37888aad0188a63d8ba1d96426d76f1299430b5
guitarbien <guitarbien@gmail.com>
2018-01-03 00:09:30 +0800

    test case 可以 build pc & laptop

---
+++ tests/BuilderTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\computer\builder\PCBuilder;
+use App\computer\builder\LaptopBuilder;
+use App\computer\Director;
+use App\computer\product\PC;
+use App\computer\product\Laptop;
+
+/**
+ * Class BuilderTest
+ */
+class BuilderTest extends TestCase
+{
+    public function test_可以打造PC()
+    {
+        // arrange
+        $pcBuilder = new PCBuilder();
+
+        // act
+        $pc = (new Director())->build($pcBuilder);
+
+        // assert
+        $this->assertInstanceOf(PC::class, $pc);
+    }
+
+    public function test_可以打造laptop()
+    {
+        // arrange
+        $laptopBuilder = new LaptopBuilder();
+
+        // act
+        $laptop = (new Director())->build($laptopBuilder);
+
+        // assert
+        $this->assertInstanceOf(Laptop::class, $laptop);
+    }
+}
