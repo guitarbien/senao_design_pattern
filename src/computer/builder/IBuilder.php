397a51658fdb6c79719cd7e7b290defd9e0e62ef
guitarbien <guitarbien@gmail.com>
2018-01-03 00:47:10 +0800

    加上 builder methods

--- src/computer/builder/IBuilder.php
+++ src/computer/builder/IBuilder.php
@@ -2,11 +2,41 @@
 
 namespace App\computer\builder;
 
+use App\computer\IComputer;
+
 /**
  * Class IBuilder
  * @package App\computer\builder
  */
 interface IBuilder
 {
+    /**
+     * 建立 Computer 物件
+     */
+    public function createComputer(): void;
 
+    /**
+     * 設置 MB
+     */
+    public function addMotherBoard(): void;
+
+    /**
+     * 設置 CPU
+     */
+    public function addCpu(): void;
+
+    /**
+     * 設置 RAM
+     */
+    public function addRam(): void;
+
+    /**
+     * 設置 SSD
+     */
+    public function addSsd(): void;
+
+    /**
+     * @return IComputer
+     */
+    public function getComputer(): IComputer;
 }
--- src/computer/builder/PCBuilder.php
+++ src/computer/builder/PCBuilder.php
@@ -2,10 +2,59 @@
 
 namespace App\computer\builder;
 
+use App\computer\IComputer;
+
 /**
  * Class PCBuilder
  * @package App\computer\builder
  */
 class PCBuilder implements IBuilder
 {
+    /**
+     * 建立 Computer 物件
+     */
+    public function createComputer(): void
+    {
+        // TODO: Implement createComputer() method.
+    }
+
+    /**
+     * 設置 MB
+     */
+    public function addMotherBoard(): void
+    {
+        // TODO: Implement addMotherBoard() method.
+    }
+
+    /**
+     * 設置 CPU
+     */
+    public function addCpu(): void
+    {
+        // TODO: Implement addCpu() method.
+    }
+
+    /**
+     * 設置 RAM
+     */
+    public function addRam(): void
+    {
+        // TODO: Implement addRam() method.
+    }
+
+    /**
+     * 設置 SSD
+     */
+    public function addSsd(): void
+    {
+        // TODO: Implement addSsd() method.
+    }
+
+    /**
+     * @return IComputer
+     */
+    public function getComputer(): IComputer
+    {
+        // TODO: Implement getComputer() method.
+    }
 }
