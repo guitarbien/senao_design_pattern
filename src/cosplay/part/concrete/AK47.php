59315da01542aae612d4656e2926f0bf20747e9d
guitarbien <guitarbien@gmail.com>
2018-01-16 23:04:48 +0800

    實作 warcraft costume

---
+++ src/cosplay/part/concrete/AK47.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\cosplay\part\concrete;
+
+use App\cosplay\part\definition\Weapon;
+
+/**
+ * Class AK47
+ * @package App\cosplay\part\concrete
+ */
+class AK47 extends Weapon
+{
+
+}
