da84b173d0cba91397368d26eec3958b3f694c26
guitarbien <guitarbien@gmail.com>
2018-01-16 22:34:02 +0800

    test case 可以生產魔獸爭霸服裝

--- tests/CosplayTest.php
+++ tests/CosplayTest.php
@@ -7,6 +7,7 @@ use App\cosplay\part\definition\Helmet;
 use App\cosplay\part\definition\Shoe;
 use App\cosplay\part\definition\Weapon;
 use App\cosplay\PokemonFactory;
+use App\cosplay\WarcraftFactory;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -17,6 +18,19 @@ class CosplayTest extends TestCase
     public function test_可以生產寶可夢服裝()
     {
         $factory = new PokemonFactory();
+        $costume = $factory->createCostume();
+
+        static::assertInstanceOf(AbstractCostume::class, $costume);
+        static::assertInstanceOf(Hat::class, $costume->getHat());
+        static::assertInstanceOf(Helmet::class, $costume->getHelmet());
+        static::assertInstanceOf(Cloth::class, $costume->getCloth());
+        static::assertInstanceOf(Shoe::class, $costume->getShoe());
+        static::assertInstanceOf(Weapon::class, $costume->getWeapon());
+    }
+
+    public function test_可以生產魔獸爭霸服裝()
+    {
+        $factory = new WarcraftFactory();
         $costume = $factory->createCostume();
 
         static::assertInstanceOf(AbstractCostume::class, $costume);
