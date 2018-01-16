871c8bcfef4e4db60a5728e3688b7d4d106db697
guitarbien <guitarbien@gmail.com>
2018-01-16 21:14:55 +0800

    test case 可以生產寶可夢服裝

---
+++ tests/CosplayTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class CosplayTestTest
+ */
+class CosplayTest extends TestCase
+{
+    public function test_可以生產寶可夢服裝()
+    {
+        $factory = new PokemonFactory();
+        $costume = $factory->createCostume();
+
+        static::assertInstanceOf(PokemonCostume::class, $costume);
+    }
+}
