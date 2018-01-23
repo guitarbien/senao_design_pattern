0d7f1d08d7c67d165d28d7cfcc28b3758d0df49e
guitarbien <guitarbien@gmail.com>
2018-01-23 22:55:06 +0800

    StrategyTest
    test case :
    訂單可以使用滿千送百活動、訂單可以使用全館八折活動、訂單可以自動選用對消費者最優惠促銷活動_全館八折較優、訂單可以自動選用對消費者最優惠促銷活動
    _滿千送百較優

---
+++ tests/StrategyTest.php
@@ -0,0 +1,100 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class StrategyTest
+ */
+class StrategyTest extends TestCase
+{
+    public function test_訂單可以使用滿千送百活動()
+    {
+        // arrange
+        $productA = new Product('sno001A', 110);
+        $productB = new Product('sno002B', 95);
+        $productC = new Product('sno003C', 675);
+
+        $minusHundredPlan = new MinusHundredPlan(1000);
+
+        $order = new Order();
+
+        // act
+        $order->addItem($productA, 2)
+              ->addItem($productB, 3)
+              ->addItem($productC)
+              ->addItem($productC)
+              ->addDiscountPlan($minusHundredPlan);
+
+        // assert
+        $expected = (110 * 2 + 95 * 3 + 675 * 2) - 100;
+        static::assertEquals($expected, $order->cauculateTotalPrice());
+    }
+
+    public function test_訂單可以使用全館八折活動()
+    {
+        // arrange
+        $productA = new Product('sno001A', 110);
+        $productB = new Product('sno002B', 95);
+        $productC = new Product('sno003C', 675);
+
+        $percentOffPlan = new PercentOffPlan(20);
+
+        $order = new Order();
+
+        // act
+        $order->addItem($productA, 2)
+              ->addItem($productB, 3)
+              ->addItem($productC)
+              ->addItem($productC)
+              ->addDiscountPlan($percentOffPlan);
+
+        // assert
+        $expected = (110 * 2 + 95 * 3 + 675 * 2) * 0.8;
+        static::assertEquals($expected, $order->cauculateTotalPrice());
+    }
+
+    public function test_訂單可以自動選用對消費者最優惠促銷活動_全館八折較優()
+    {
+        // arrange
+        $productA = new Product('sno001A', 110);
+        $productB = new Product('sno002B', 95);
+        $productC = new Product('sno003C', 675);
+
+        $minusHundredPlan = new MinusHundredPlan(1000);
+        $percentOffPlan   = new PercentOffPlan(20);
+
+        $order = new Order();
+
+        // act
+        $order->addItem($productA, 2)
+              ->addItem($productB, 3)
+              ->addItem($productC)
+              ->addItem($productC)
+              ->addDiscountPlan($minusHundredPlan)
+              ->addDiscountPlan($percentOffPlan);
+
+        // assert
+        $expected = (110 * 2 + 95 * 3 + 675 * 2) * 0.8;
+        static::assertEquals($expected, $order->cauculateTotalPrice());
+    }
+
+    public function test_訂單可以自動選用對消費者最優惠促銷活動_滿千送百較優()
+    {
+        // arrange
+        $productA = new Product('sno001A', 110);
+
+        $minusHundredPlan = new MinusHundredPlan(1000);
+        $percentOffPlan   = new PercentOffPlan(20);
+
+        $order = new Order();
+
+        // act
+        $order->addItem($productA, 10)
+              ->addDiscountPlan($minusHundredPlan)
+              ->addDiscountPlan($percentOffPlan);
+
+        // assert
+        $expected = (110 * 10) - 100;
+        static::assertEquals($expected, $order->cauculateTotalPrice());
+    }
+}
