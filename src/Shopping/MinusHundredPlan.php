9502fbcdc08fb590a1d70680a3e2e7ee5401c1bc
guitarbien <guitarbien@gmail.com>
2018-01-23 23:51:50 +0800

    MinusHundredPlan construct 時紀錄此活動滿幾千才送，實作滿千送百計算

--- src/Shopping/MinusHundredPlan.php
+++ src/Shopping/MinusHundredPlan.php
@@ -8,12 +8,24 @@ namespace App\Shopping;
  */
 class MinusHundredPlan implements DiscountPlan
 {
+    /** @var int "幾"千送幾百 */
+    private $baseline;
+
+    /**
+     * MinusHundredPlan constructor.
+     * @param int $baseline
+     */
+    public function __construct(int $baseline)
+    {
+        $this->baseline = $baseline;
+    }
+
     /**
      * @param int $originalPrice
      * @return int
      */
     public function calculateTotalPrice(int $originalPrice): int
     {
-        return $originalPrice;
+        return $originalPrice - (floor($originalPrice / $this->baseline) * 100);
     }
 }
