0a18122f91bfc8490950fdd8e95ae656cd97a45a
guitarbien <guitarbien@gmail.com>
2018-02-22 23:21:56 +0800

    建立基本的 setNext() 和 validate() method

--- src/Validator/AlphabetValidator.php
+++ src/Validator/AlphabetValidator.php
@@ -8,4 +8,21 @@ namespace App\Validator;
  */
 final class AlphabetValidator
 {
+    /**
+     * @param $param
+     * @return AlphabetValidator
+     */
+    public function setNext($param): self
+    {
+        return $this;
+    }
+
+    /**
+     * @param string $idNumber
+     * @return bool
+     */
+    public function validate(string $idNumber): bool
+    {
+        return true;
+    }
 }
