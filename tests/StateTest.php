573c1cf597cd89c2bed45db5587b8cf2b826dfd5
guitarbien <guitarbien@gmail.com>
2018-03-09 00:20:41 +0800

    create test case
    紅綠燈號誌＿初始為綠燈
    紅綠燈號誌＿初始後變化一次為黃燈
    紅綠燈號誌＿初始後變化兩次為紅燈
    紅綠燈號誌＿初始後變化三次為綠燈

---
+++ tests/StateTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use App\StatePattern\TrafficLight;
+use App\StatePattern\State;
+
+/**
+ * Class StateTest
+ */
+class StateTest extends TestCase
+{
+    public function test_紅綠燈號誌＿初始為綠燈()
+    {
+        // arrange
+        $trafficLight = new TrafficLight();
+
+        // act
+        $status = $trafficLight->getState();
+
+        // assert
+        static::assertEquals($status, new State(State::GREEN));
+    }
+
+    public function test_紅綠燈號誌＿初始後變化一次為黃燈()
+    {
+        // arrange
+        $trafficLight = new TrafficLight();
+
+        // act
+        $trafficLight->change();
+
+        $status = $trafficLight->getState();
+
+        // assert
+        static::assertEquals($status, State::YELLOW);
+    }
+
+    public function test_紅綠燈號誌＿初始後變化兩次為紅燈()
+    {
+        // arrange
+        $trafficLight = new TrafficLight();
+
+        // act
+        $trafficLight->change();
+        $trafficLight->change();
+
+        $status = $trafficLight->getState();
+
+        // assert
+        static::assertEquals($status, State::RED);
+    }
+
+    public function test_紅綠燈號誌＿初始後變化三次為綠燈()
+    {
+        // arrange
+        $trafficLight = new TrafficLight();
+
+        // act
+        $trafficLight->change();
+        $trafficLight->change();
+        $trafficLight->change();
+
+        $status = $trafficLight->getState();
+
+        // assert
+        static::assertEquals($status, State::GREEN);
+    }
+}
