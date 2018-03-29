09d774277088dba8144b78fa3f8bceae0d1614db
guitarbien <guitarbien@gmail.com>
2018-03-30 00:42:26 +0800

    test case 刷中信卡滿千送百全館八折送百元折價券

---
+++ tests/DecoratorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Decorator\BankType;
+use PHPUnit\Framework\TestCase;
+use App\Decorator\CreditCard;
+
+/**
+ * Class DecoratorTest
+ */
+class DecoratorTest extends TestCase
+{
+    public function test_刷中信卡滿千送百全館八折送百元折價券()
+    {
+        $creditCard = new CreditCard(BankType::CTBC);
+        $order = $creditCard->checkOut(1100);
+
+        static::assertEquals($order->getTotalPrice(), 800);
+        static::assertTrue($order->isPlusOneEvent());
+    }
+}
