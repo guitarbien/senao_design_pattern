e73eb393211da9a59e73e65e47a30029f54eb7bc
guitarbien <guitarbien@gmail.com>
2018-04-13 00:18:40 +0800

    針對有條件的 decorator 套上 proxy，透過 null object 讓行為一致減少 if else

--- src/Decorator/CreditCard.php
+++ src/Decorator/CreditCard.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\Decorator;
 
+use App\Decorator\ConditionProxy\Customer;
+use App\Decorator\ConditionProxy\PromotionPeriod;
+use App\Decorator\EventDecorator\AbstractEventDecorator;
 use App\Decorator\EventDecorator\BonusPointDecorator;
 use App\Decorator\EventDecorator\CouponDecorator;
+use App\Decorator\EventDecorator\EventDecorator;
 use App\Decorator\EventDecorator\OriginalEventDecorator;
 use App\Decorator\EventDecorator\PlusOneDecorator;
 use App\Decorator\Functional\PriceMinusHundred;
@@ -13,6 +17,7 @@ use App\Decorator\Functional\PriceOriginal;
 use App\Decorator\Functional\PriceTwentyPercentOff;
 use App\Decorator\PriceDecorator\MinusHundredDecorator;
 use App\Decorator\PriceDecorator\OriginalPricePriceDecorator;
+use App\Decorator\PriceDecorator\PriceDecoratorInterface;
 use App\Decorator\PriceDecorator\TwentyPercentOffDecorator;
 
 /**
@@ -24,13 +29,50 @@ final class CreditCard
     /** @var string */
     private $bank;
 
+    /** @var Customer */
+    private $customer;
+
+    /** @var PromotionPeriod */
+    private $promotionPeriod;
+
     /**
      * CreditCard constructor.
      * @param string $bank
+     * @param Customer $customer
+     * @param PromotionPeriod $promotionPeriod
      */
-    public function __construct(string $bank)
+    public function __construct(string $bank, Customer $customer, PromotionPeriod $promotionPeriod)
     {
-        $this->bank = $bank;
+        $this->bank            = $bank;
+        $this->customer        = $customer;
+        $this->promotionPeriod = $promotionPeriod;
+    }
+
+    /**
+     * @param AbstractEventDecorator $eventDecorator
+     * @return EventDecorator
+     */
+    private function proxyCouponDecorator(EventDecorator $eventDecorator): EventDecorator
+    {
+        return $this->customer->isMember() ? new CouponDecorator($eventDecorator) : new NullEventDecorator($eventDecorator);
+    }
+
+    /**
+     * @param PriceDecoratorInterface $priceDecorator
+     * @return PriceDecoratorInterface
+     */
+    private function proxyTwentyPercentOffDecorator(PriceDecoratorInterface $priceDecorator):  PriceDecoratorInterface
+    {
+        return $this->customer->isVipMember() ? new TwentyPercentOffDecorator($priceDecorator) : new NullPriceDecorator($priceDecorator);
+    }
+
+    /**
+     * @param EventDecorator $eventDecorator
+     * @return EventDecorator
+     */
+    private function proxyPlusOneDecorator(EventDecorator $eventDecorator): EventDecorator
+    {
+        return $this->customer->isMember() ? new PlusOneDecorator($eventDecorator) : new NullEventDecorator($eventDecorator);
     }
 
     /**
@@ -42,19 +84,18 @@ final class CreditCard
         $order = new Order();
 
         // calculate price
-        $totalPrice = (new TwentyPercentOffDecorator(
-                            new MinusHundredDecorator(
-                                new OriginalPricePriceDecorator($this->bank))))
-                                    ->getPrice($totalPrice);
+        $totalPrice = $this->proxyTwentyPercentOffDecorator(
+                                new MinusHundredDecorator(
+                                    new OriginalPricePriceDecorator($this->bank)))
+                                        ->getPrice($totalPrice);
 
         $order->setTotalPrice($totalPrice);
 
         // handle events
-        $events = (new PlusOneDecorator(
+        $events = $this->proxyPlusOneDecorator(
                     new BonusPointDecorator(
-                        new CouponDecorator(
-                            new NullEventDecorator($this->bank)))))
-                                ->getEvents([]);
+                        $this->proxyCouponDecorator(new OriginalEventDecorator($this->bank))))
+                            ->getEvents([]);
 
         $order->setEvents($events);
 
--- tests/DecoratorTest.php
+++ tests/DecoratorTest.php
@@ -3,20 +3,36 @@
 declare(strict_types=1);
 
 use App\Decorator\BankType;
+use App\Decorator\ConditionProxy\Customer;
+use App\Decorator\ConditionProxy\PromotionPeriod;
+use App\Decorator\CreditCard;
+use App\Decorator\Events\BonusPoint;
 use App\Decorator\Events\Coupon;
 use App\Decorator\Events\PlusOne;
-use App\Decorator\Events\BonusPoint;
 use PHPUnit\Framework\TestCase;
-use App\Decorator\CreditCard;
 
 /**
  * Class DecoratorTest
  */
 class DecoratorTest extends TestCase
 {
+    /** @var Customer */
+    private $customer;
+
+    /** @var PromotionPeriod */
+    private $promotionPirod;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->customer       = new Customer();
+        $this->promotionPirod = new PromotionPeriod();
+    }
+
     public function test_刷中信卡滿千送百全館八折_functional()
     {
-        $creditCard = new CreditCard(BankType::CTBC);
+        $creditCard = new CreditCard(BankType::CTBC, $this->customer, $this->promotionPirod);
         $order = $creditCard->functionalCheckOut(1100);
 
         static::assertEquals($order->getTotalPrice(), 800);
@@ -24,7 +40,7 @@ class DecoratorTest extends TestCase
 
     public function test_刷中信卡滿千送百全館八折送百元折價券()
     {
-        $creditCard = new CreditCard(BankType::CTBC);
+        $creditCard = new CreditCard(BankType::CTBC, $this->customer, $this->promotionPirod);
         $order = $creditCard->checkOut(1100);
 
         static::assertEquals($order->getTotalPrice(), 800);
@@ -36,7 +52,7 @@ class DecoratorTest extends TestCase
 
     public function test_刷台新卡全館八折加一元多一件送百元折價券()
     {
-        $creditCard = new CreditCard(BankType::TAISHIN);
+        $creditCard = new CreditCard(BankType::TAISHIN, $this->customer, $this->promotionPirod);
         $order = $creditCard->checkOut(1100);
 
         static::assertEquals($order->getTotalPrice(), 880);
@@ -52,7 +68,7 @@ class DecoratorTest extends TestCase
 
     public function test_刷花旗卡滿千送百紅利點數一百點加一元多一件()
     {
-        $creditCard = new CreditCard(BankType::CITI);
+        $creditCard = new CreditCard(BankType::CITI, $this->customer, $this->promotionPirod);
         $order = $creditCard->checkOut(1100);
 
         static::assertEquals($order->getTotalPrice(), 1000);
